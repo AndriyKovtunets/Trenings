0e1e71a93cc672aad497152435d648ecb2f566c9
Andriy Kovtunets <andriy.kovtunets@gmail.com>
2016-05-07 00:07:59 +0300

    fixed

--- Practice1/src/ua/nure/kovtunets/Practice1/Task4.java
+++ Practice1/src/ua/nure/kovtunets/Practice1/Task4.java
@@ -5,7 +5,7 @@ public class Task4 {
 	public static void main(String[] args) {
 
 
-if(getValidation(args)==false) System.out.println("Sory ;()");
+if(getValidation(args)==false) ;
 else
 {
 			int sum = 0;
@@ -17,14 +17,18 @@ else
 	}
 
 	public static boolean getValidation (String[] args){
+		
+		final short MIN_RANGE=48; // range encodings number for ASCI table
+		final short MAX_RANGE=57;
+		
 		int c=0;
 		// додати константу
-		if (args.length != 1){ System.out.println("Pls enter 1 numbers"); return false;}
+		if (args.length != 1){ System.out.println("Pleas enter one numbers"); return false;}
 		
 		for(int i=0; i<args.length; i++){
 		for(int j=0; j<args[i].length();j++){
 			c=args[i].charAt(j); 
-		if	(c<48 || c>57) { System.out.println("Pls enter only number"); return false;}
+		if	(c<MIN_RANGE || c>MAX_RANGE) { System.out.println("Pleas enter only number"); return false;}
 		
 		}
 	}
